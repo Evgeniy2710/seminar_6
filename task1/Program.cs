c733638819cda9f92bdd6b43dc11c873fa9bd99a
Evgeniy <evalrez@mail.ru>
2024-02-01 20:31:57 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте массив символов (тип char []). Создайте строку из
+// символов этого массива.
+// Указание
+// Конструктор строки вида string(char []) не использовать.
+// Пример
+// [‘a’, ‘b’, ‘c’, ‘d’] => “abcd”
+
+string CharOffString(char[]arr)
+{
+    string st = "";
+    foreach (var item in arr)
+    {
+        st = st + item;
+    }
+    return st;
+}
+
+ char[] chars = new char[] {'a', 'b', 'c', 'd'};
+
+Console.WriteLine(CharOffString(chars));
